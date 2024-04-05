380b30910c8095ef19711a95d1c3eaa7156b88e5
Octavio Guevara <112457564+ProyectosOctavio@users.noreply.github.com>
2024-04-05 14:13:08 -0600

    D_proveedor

---
+++ TiendaDeRopa/Datos/D_Proveedor.cs
@@ -0,0 +1,46 @@
+﻿using TiendaDeRopa.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaDeRopa.Datos
+{
+    public class D_Productos
+    {
+
+        public DataTable Listado_prov(string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_LISTADO_PROV", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+
+
+        }
+    }
+}
