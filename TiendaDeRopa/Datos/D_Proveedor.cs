129c9503785402acb5798db254b46cf7fc50812d
Octavio Guevara <112457564+ProyectosOctavio@users.noreply.github.com>
2024-04-05 14:41:59 -0600

    Guardar

--- TiendaDeRopa/Datos/D_Proveedor.cs
+++ TiendaDeRopa/Datos/D_Proveedor.cs
@@ -42,5 +42,36 @@ namespace TiendaDeRopa.Datos
 
 
         }
+
+        public string Guardar_prov(int nOpcion,
+                               E_Proveedor oProv)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_GUARDAR_PROV", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@opcion", SqlDbType.Int).Value = nOpcion;
+                Comando.Parameters.Add("@id", SqlDbType.Int).Value = oProv.id;
+                Comando.Parameters.Add("@nombre", SqlDbType.VarChar).Value = oProv.nombre;
+                Comando.Parameters.Add("@email", SqlDbType.VarChar).Value = oProv.email;
+                Comando.Parameters.Add("@telefono", SqlDbType.VarChar).Value = oProv.telefono;
+                Comando.Parameters.Add("@direccion", SqlDbType.VarChar).Value = oProv.direccion;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() >= 1 ? "OK" : "No se pudo registrar los datos";
+            }
+
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
     }
 }
