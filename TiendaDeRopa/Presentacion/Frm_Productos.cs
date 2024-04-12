cf3723f3476019b58801b3741f544a3d274b7dfd
JanCarlosPM <jan.prado13373@est.uca.edu.ni>
2024-04-12 17:06:44 -0600

    Diseño listo de productos

--- TiendaDeRopa/Presentacion/Frm_Productos.cs
+++ TiendaDeRopa/Presentacion/Frm_Productos.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TiendaDeRopa.Datos;
 
 namespace TiendaDeRopa.Presentacion
 {
     public partial class Frm_Productos : Form
     {
+
+        private DataTable datosOriginales;
         public Frm_Productos()
         {
             InitializeComponent();
+            ListarProductos();
+        }
+
+        private void ListarProductos()
+        {
+            D_Producto productos = new D_Producto();
+            datosOriginales = productos.ListarProductos();
+            dgvProductos.DataSource = datosOriginales;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_Productos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
